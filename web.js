94c08692eeeb2ca3df8af1bfa8036c4c61a8efc5
Raphael Barreto <rfsbarreto@gmail.com>
2013-07-11 22:37:55 -0300

    testando buffer

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var str= fs.readFileSync('index.html','utf-8');
-//console.log(str);
+console.log(str);
 
 //var buf= new Buffer(str,'utf-8');
 
