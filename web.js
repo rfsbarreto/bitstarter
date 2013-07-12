4608332213a80aa326f8cf5236e913fe00383839
Raphael Barreto <rfsbarreto@gmail.com>
2013-07-11 22:33:15 -0300

    testando buffer

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var str= fs.readFileSync('index.html');
+var str= fs.readFileSync('index.html','utf-8');
+//console.log(str);
+
 //var buf= new Buffer(str,'utf-8');
 
 
 
 app.get('/', function(request, response) {
-  response.send(str.toString)
+  response.send(str)
 
 port = process.env.PORT || 5000;
 app.listen(port, function() {
