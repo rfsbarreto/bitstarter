8996f74cd0509d2ac3b579e7328a4215be54f8f4
Raphael Barreto <rfsbarreto@gmail.com>
2013-07-11 22:12:45 -0300

    web.js readig index

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var str= fs.readFileSync('index.html');
+//var buf= new Buffer(str,'utf-8');
 
-var port = process.env.PORT || 5000;
+
+
+app.get('/', function(request, response) {
+  response.send(str.toString)
+
+port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
