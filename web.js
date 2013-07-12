116b17cdc07887f5663bfdeab11e3cab024fa3d5
Raphael Barreto <rfsbarreto@gmail.com>
2013-07-11 23:17:46 -0300

    Correcao do web.js 1

--- web.js
+++ web.js
@@ -3,15 +3,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var str= fs.readFileSync('index.html','utf-8');
+
 //console.log(str);
 
-var buf= new Buffer(str,'utf-8');
+
+//var buf= new Buffer(str,'utf-8');
 
 
 
 app.get('/', function(request, response) {
-  response.send(str);
+  response.send(fs.readFileSync('index.html','utf-8'));
 });
 
 
